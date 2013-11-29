3a8ea9ef95f04e98c1fb44fb2d58080bd554c845
James Pickering <james_pic@hotmail.com>
2013-11-29 15:03:04 +0000

    Fix to check_selenium

--- dist/check_selenium.sh
+++ dist/check_selenium.sh
@@ -2,8 +2,8 @@
 
 JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386/jre
 DIR=$(/usr/bin/dirname ${0})
-CLASSPATH=${DIR}/lib/*
+CLASSPATH=${DIR}/lib/*:${DIR}
 
 #echo $CLASSPATH
 
-./ephemeral-x.sh -q1 ${JAVA_HOME}/bin/java -cp ${CLASSPATH} info.devopsabyss.CallSeleniumTest $@
+./ephemeral-x.sh -q ${JAVA_HOME}/bin/java -cp ${CLASSPATH} info.devopsabyss.CallSeleniumTest $@

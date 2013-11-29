720b256ae92bb984203ce2f80d1b5d2fa84e223d
James Pickering <james_pic@hotmail.com>
2013-11-29 15:21:10 +0000

    Removed JAVA_HOME stuff - no point under Linux

--- dist/check_selenium.sh
+++ dist/check_selenium.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386/jre
 DIR=$(/usr/bin/dirname ${0})
 CLASSPATH=${DIR}/lib/*:${DIR}
 
 #echo $CLASSPATH
 
-./ephemeral-x.sh -q ${JAVA_HOME}/bin/java -cp ${CLASSPATH} info.devopsabyss.CallSeleniumTest $@
+./ephemeral-x.sh -q java -cp ${CLASSPATH} info.devopsabyss.CallSeleniumTest $@
